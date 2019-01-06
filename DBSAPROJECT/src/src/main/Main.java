973a17b4b9353e303c4d46afbf11fb096d530742
Nezrin <nezrin.necefzade@gmail.com>
2019-01-06 20:47:05 +0100

    Update Main.java

--- DBSAPROJECT/src/src/main/Main.java
+++ DBSAPROJECT/src/src/main/Main.java
@@ -8,51 +8,5 @@ import src.main.StreamDriver;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		// firstbenchmarking
-
-		int numberOfStreams = 1;
-		int methodType = 4;
-		int bufferSize = 1024 * 16;
-		int minFileSize = 1;
-		int maxFileSize = 1000; 
-		String operationType = "W"; //When change operationType, change the last parameter of StreamDriver's constructor
-		int maxK = 30;
-
-		long start;
-		long end;
-		long duration;
-		double seconds;
-
-		new File("streamFiles/Files/").mkdirs();
-
-		int u = 2;
-		for (int fileSize = minFileSize; fileSize <= maxFileSize; fileSize *= u) {
-			u = u == 5 ? 2 : 5;
-			String writing_path = "streamFiles\\Files\\" + fileSize + "_";
-
-			String[] reading_path = new String[maxK];
-			
-			for (int j = 0; j < maxK; j++) {
-				reading_path[j] = writing_path + j + ".txt";
-			}
-
-			for (int i = 0; i <= maxK; i += 5) {
-				if (i == 0)
-					numberOfStreams = 1;
-				else
-					numberOfStreams = i;
-
-				start = System.nanoTime();
-				StreamDriver driver = new StreamDriver(numberOfStreams, methodType, bufferSize, operationType, fileSize, writing_path);
-				end = System.nanoTime();
-				duration = (end - start);
-				seconds = (double) duration / 1000000000.0;
-				System.out.println("numberOfStreams = " + numberOfStreams + ", methodType = " + methodType + ", bufferSize = "
-								+ bufferSize + " , operationType = " + operationType + ", fileSize = " + fileSize);
-				System.out.println(seconds + " seconds");
-			}
-		}
-	}
 
 }
